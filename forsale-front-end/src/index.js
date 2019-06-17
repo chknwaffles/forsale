055706e9fa866c4f20bb250f40e839ff39ef24cc
Won Kim <wonsae.kim@gmail.com>
2019-06-17 15:17:39 -0400

    get modals to work

--- forsale-front-end/src/index.js
+++ forsale-front-end/src/index.js
@@ -1,27 +1,55 @@
-let contentContainer = document.getElementById('content');
+let contentContainer = document.getElementsByClassName('content-container')[0];
 let navBar =document.getElementById('nav-bar');
+const ITEMS_URL = 'http://localhost:3000/api/v1/items';
+let ITEMS_ARRAY = [];
 
-
-
-let routes = {
-    '/': homepage,
-    '/index.html': homepage,
-  };
+// let routes = {
+//     '/': homepage,
+//     '/index.html': homepage,
+//   };
 
 
 
  
   
-  navBar.addEventListener('click', (e) => {
-      console.log(e.target.innerText)
-      if(e.target.dataset.id === "home"){
-       
-          navItemClick('/')
-      }
-  }
-  )
+// navBar.addEventListener('click', (e) => {
+//     console.log(e.target.innerText)
+//     if(e.target.dataset.id === "home"){
+    
+//         navItemClick('/')
+//     }
+// })
 
-  function navItemClick(path){
-    // window.history.pushState({}, '', window.location.origin + path);
-    contentDiv.innerHTML = routes[path];
-  }
+//   function navItemClick(path){
+//     // window.history.pushState({}, '', window.location.origin + path);
+//     contentDiv.innerHTML = routes[path];
+//   }
+
+function init() {
+    fetchItems();
+}
+
+function fetchItems() {
+    fetch(ITEMS_URL)
+    .then(resp => resp.json())
+    .then(items => {
+        items.forEach(item => {
+            let newItem = new Item(item);
+            contentContainer.innerHTML += newItem.renderItem();
+            ITEMS_ARRAY.push(newItem);
+        })
+    })
+}
+
+function initEvents() {
+    contentContainer.addEventListener('click', e => {
+    });
+
+}
+
+function findItem(id) {
+    return ITEMS_ARRAY.find(item => item.id === +id);
+}
+
+init();
+
--- forsale-front-end/src/item.js
+++ forsale-front-end/src/item.js
@@ -1,5 +1,6 @@
 class Item {
-    constructor(name, description, location, images, user, comments) {
+    constructor({id, name, description, location, images, user, comments}) {
+        this.id = id;
         this.name = name;
         this.description = description
         this.location = location;
@@ -11,12 +12,12 @@ class Item {
     renderItem() {
         return `
         <div class="card mb-3">
-            <h3 class="card-header">${this.name}</h3>
+            <h3 class="card-header" data-target='#${this.name.replace(/\s/g, '')}' data-toggle='modal'>${this.name}</h3>
             <div class="card-body">
                 <h5 class="card-title">${this.location}</h5>
                 <h6 class="card-subtitle text-muted">${this.tags}</h6>
             </div>
-            <img style="height: 200px; width: 100%; display: block;" src="${this.images[0]}" alt="Card image">
+            <img style="height: 200px; width: 200px;" src="${this.images[0]}" alt="Card image">
             <div class="card-body">
                 <p class="card-text">${this.description}</p>
             </div>
@@ -27,32 +28,33 @@ class Item {
                 2 days ago
             </div>
         </div>
+        ${this.renderItemOnClick()}
         `
     }
 
-    static renderItemOnClick(item) {
+    renderItemOnClick() {
         return `
-        <div class="modal">
-            <div class="modal-dialog" role="document">
+        <div class="modal" id='${this.name.replace(/\s/g, '')}' tabindex='-1' role='dialog'>
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">${item.name}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>${item.description}</p>
-                    <img src=${item.images[0]}>
-                    <img src=${item.images[1]}>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <div class="modal-header">
+                        <h5 class="modal-title">${this.name}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>${this.description}</p>
+                        <img src=${this.images[0]}>
+                        <img src=${this.images[1]}>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
         `
     }
 }
