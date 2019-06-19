bb6af305d750891d9570a6ee5d0d57ee0d689ad7
Yasmine <y.hartung@gmail.com>
2019-06-19 17:18:26 -0400

    deleteing from API. not closing modal

--- forsale/config/routes.rb
+++ forsale/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      delete "/items/:id", to: "items#destroy"
       resources :items, only: [:index, :create, :update]
       resources :users, only: [:index, :create, :update]
       resources :comments, only: [:index, :create, :update]
