552ee440008828311beed55ddd4d5c7512d1724c
Won Kim <wonsae.kim@gmail.com>
2019-06-19 14:00:24 -0400

    navbar stuff and alerts

--- forsale-front-end/src/index.js
+++ forsale-front-end/src/index.js
@@ -5,7 +5,7 @@ let contentContainer = document.getElementsByClassName('jumbotron')[0];
 let navBar = document.getElementById('nav-bar');
 let signInForm = document.getElementById('sign-in-form');
 let ITEMS_ARRAY = [];
-let current_user = '';
+let current_user = new User({});
 
 function init() {
     fetchItems();
@@ -23,20 +23,17 @@ function fetchItems() {
         })
     })
 }
-    
-//infinite scroll
+
 function initEvents() {
-    contentContainer.addEventListener('scroll', e => {
-    });
+    //infinite scroll
+    // contentContainer.addEventListener('scroll', e => {
+    // });
 
     document.getElementsByClassName('form-control mr-sm-2')[0].addEventListener('input', e => {
         let allCards = document.getElementsByClassName('card mb-3');
 
         for(let card of allCards) {
-            if (card.getElementsByTagName('span')[0].innerText.includes(e.target.value))
-                card.style.display = '';
-            else
-                card.style.display = 'none';
+            card.style.display = (card.getElementsByTagName('span')[0].innerText.includes(e.target.value)) ? '' : 'none';
         }
     })
 
@@ -53,35 +50,39 @@ function initEvents() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({username, email})
-        }).then(r => r.json())
+        })
+        .then(r => r.json())
         .then(user => {
             current_user = new User(user);
-            navBar.getElementsByClassName('navbar-nav mr-auto')[0].children[3].style.display = 'none';
             //prepend the success alert to the top
-            document.querySelector('body').prepend(current_user.success())
+            document.querySelector('body').prepend(current_user.toastMsg(`Welcome ${current_user.username}! You have successfully logged in.`))
             //unfortunately have to use jquery to close the login modal and show toast
             $('#sign-in').modal('hide');
             $('.toast').toast('show');
+            //show add page and user show page to navbar
+            current_user.loggedIn(navBar.getElementsByClassName('navbar-nav mr-auto')[0]);
         })
     })
 
     contentContainer.addEventListener('click', e => {
         if (e.target.id === 'add-comment') {
-            if (current_user !== '') {
+            if (current_user.username !== '') {
                 //logged in so let's show a form to add comment
-                e.target.parentElement.previousElementSibling.style.display = "";
+                let addCommentForm = e.target.parentElement.previousElementSibling
+                addCommentForm.style.display = "";
                 //possibly change the button to submit comment
                 e.target.innerText = 'Submit'
                 setTimeout(() => {
                     e.target.id = 'submit-comment';
-                }, 5000);
+                }, 3000);
             } else {
                 //alert to log in to add a comment
+                document.querySelector('body').prepend(current_user.toastMsg('You need to login to comment!'))
+                $('.toast').toast('show');
             }
         }
 
         if(e.target.id === 'submit-comment') {
-            debugger
             let comment = e.target.parentElement.previousElementSibling.firstElementChild[0].value;
             let item_id = e.target.offsetParent.parentElement.parentElement.id.split('-')[2];
             fetch(COMMENTS_URL, {
@@ -94,8 +95,11 @@ function initEvents() {
             })
             .then(r => r.json())
             .then(comment => {
-                //render on dom
-                
+                //render on dom and change submit button to add comment and get rid of textfield
+
+                setTimeout(() => {
+                    e.target.id = 'add-comment';
+                }, 3000);
             })
             .catch(console.log(err))
         }
