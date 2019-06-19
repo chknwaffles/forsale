448357e3d4ad72e0920127f20dac5528e5535515
Won Kim <wonsae.kim@gmail.com>
2019-06-19 14:17:15 -0400

    fix seed data

--- forsale/db/schema.rb
+++ forsale/db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_06_19_171947) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
   end
 
   create_table "items_tags", id: false, force: :cascade do |t|
